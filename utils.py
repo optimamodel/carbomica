d6b7d262705a13b2252095145828ee4e7601956d
Romesh Abeysuriya <romesh.abey@gmail.com>
2024-07-08 13:17:29 +1000

    Remove unused function

--- utils.py
+++ utils.py
@@ -130,53 +130,6 @@ def plot_emissions(df: pd.DataFrame, title:str='Total CO2e Emissions') -> plt.Fi
     return fig
 
 
-def write_alloc_excel(progset, results, year, print_results=True,file_name=None):
-    """Write optimized budget allocations onto an excel file
-        :param: P: atomica project
-        :param: results: results from optimization runs
-        :param: save_dir: path for saving the plot
-        :param: name to be given to excel file (string)
-        """
-        
-    progname = []
-    prog_labels = []
-    for prog in progset.programs:
-        progname += [prog]
-        prog_labels += [progset.programs[prog].label]
-        
-    bars = []
-    for i in range(0, len(results)):
-         bar_name = results[i].name
-         bars.append(bar_name)
-         
-    d1 = at.PlotData.programs(results, quantity='spending')
-    d1.interpolate(year)
-    spending_raw_data = {(x.result, x.output): x.vals[0] for x in d1.series}
-    spending_data = {res: {prog:0 for prog in progname} for res in bars}
-    
-    d2 = at.PlotData.programs(results, quantity='coverage_fraction')
-    d2.interpolate(year)
-    cov_raw_data = {(x.result, x.output): x.vals[0] for x in d2.series}
-    cov_data = {res: {prog:0 for prog in progname} for res in bars}
-    for br in bars:
-        for prog in progname:
-            spending_data[br][prog] = spending_raw_data[(br, prog)]
-            cov_data[br][prog] = cov_raw_data[(br, prog)]
-    df1 = pd.DataFrame(spending_data)
-    df2 = pd.DataFrame(cov_data)
-    df1.index = prog_labels
-    df2.index = prog_labels
-    
-    if print_results:
-        file_name = file_name+'.xlsx'
-        writer = pd.ExcelWriter(file_name, engine='xlsxwriter')
-        df1.to_excel(writer, sheet_name="Budgets")
-        df2.to_excel(writer, sheet_name="Coverages")
-        writer.close()
-        print('Excel file saved: {}'.format(file_name))
-    
-    return df1, df2
-
 def powerset(set):
     """
     Return all combinations of length 0-n
